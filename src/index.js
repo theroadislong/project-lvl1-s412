8d758c6bf07a5c2e77ea5fd5944d9bbd2b63c04a
Artem <dunaev.aa@yandex.ru>
2019-01-15 00:56:32 +0700

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function greetingUser() {
-  const userName = readlineSync.question("May I have your name? : ");
-  console.log("Hello, ", userName);
+  const userName = readlineSync.question('May I have your name? : ');
+  console.log('Hello, ', userName);
 }
 
 export default greetingUser;
