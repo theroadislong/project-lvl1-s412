352257ab831a343097cf7dd547c65be985aeb4b6
Artem <dunaev.aa@yandex.ru>
2019-01-18 22:20:51 +0700

    fix gameProcess

--- src/index.js
+++ src/index.js
@@ -12,18 +12,18 @@ const greetingUser = (description = '') => {
 
 const gameProcess = (description = '', runGameEven) => {
   const userName = greetingUser(description);
-  const getResult = (rightAnswer, userAnswer) => {
-    if (rightAnswer === userAnswer) {
-      console.log('Correct!');
-      return true;
-    }
-    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}"`);
-    console.log(`Let's try again, ${userName}!`);
-    return false;
-  };
   for (let i = 1; i <= gameRounds; i += 1) {
     const { question, rightAnswer } = runGameEven();
     const userAnswer = readlineSync.question(`Question: ${question} `);
+    const getResult = (right, user) => {
+      if (right === user) {
+        console.log('Correct!');
+        return true;
+      }
+      console.log(`"${user}" is wrong answer ;(. Correct answer was "${right}"`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    };
     if (!getResult(rightAnswer, userAnswer)) {
       return;
     }
