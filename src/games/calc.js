998191616e1cfc73b34a73d97b1b41206d37178a
Artem <dunaev.aa@yandex.ru>
2019-01-18 11:30:46 +0700

    fix calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,3 @@
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../utils';
 import { gameProcess } from '..';
 
@@ -23,16 +22,17 @@ const obj = {
 const arrOfFunctions = ['sum', 'sub', 'mult'];
 const getRandomFunction = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const first = getRandomNumber(minNumber, maxNumber);
-const second = getRandomNumber(minNumber, maxNumber);
-const randSymbol = getRandomFunction(arrOfFunctions);
-const question = `${first} ${obj[randSymbol].symbol} ${second}`;
 const description = 'What is the result of the expression?';
 
 const runGameCalc = () => {
+  const first = getRandomNumber(minNumber, maxNumber);
+  const second = getRandomNumber(minNumber, maxNumber);
+  const randSymbol = getRandomFunction(arrOfFunctions);
+  const question = `${first} ${obj[randSymbol].symbol} ${second}`;
   const rightAnswer = obj[randSymbol].func(first, second).toString();
-  const userAnswer = readlineSync.question(`Question: ${question} `);
-  gameProcess(description, rightAnswer, userAnswer);
+  return { question, rightAnswer };
 };
+
+gameProcess(description, runGameCalc);
 
 export default runGameCalc;
